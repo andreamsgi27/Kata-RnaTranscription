4e52b942374c94b231a6d6d39df8417c7c5f87ff
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-25 18:28:43 +0100

    Implement a loop to transcribe the strand using an array of chars and returning the new String transcribed. All testes provided pass.

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -4,7 +4,25 @@ package org.factoriaf5.rna_transcription;
 public class RnaTranscription {
     
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
+        //throw new UnsupportedOperationException("Implement method");
+        char[] dnaChars = dnaStrand.toCharArray();
+        char[] dnaTranscripted = new char[(dnaChars.length)];
+
+        for (int i = 0; i < dnaChars.length; i++){
+            if (dnaChars[i] == 'C') {
+                dnaTranscripted[i] = 'G';
+            } else if (dnaChars[i] == 'G') {
+                dnaTranscripted[i] = 'C';
+            } else if (dnaChars[i] == 'T') {
+                dnaTranscripted[i] = 'A';
+            } else if (dnaChars[i] == 'A') {
+                dnaTranscripted[i] = 'U';
+            } else {
+                dnaTranscripted[i] = dnaChars[i];
+            }
+        }
+
+        return new String(dnaTranscripted);
     }
 
 }
